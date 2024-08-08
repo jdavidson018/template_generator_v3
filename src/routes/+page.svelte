7efd1eb30485df5fe61219b65dbffc4e5a95e58c
Jack Davidson <jdavidson018@gmail.com>
2024-08-07 22:17:35 -0600

    feat: Added Tailwind and Initial Layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-blue-950 min-h-screen">
+	<div class="container mx-auto h-screen flex flex-col">
+		<div class="flex justify-between h-12 bg-blue-200">
+			<h2 class="ml-4 flex items-center">Template Generator V3</h2>
+			<h4 class="mr-4 flex items-center">New Template</h4>
+		</div>
+		<div class="flex-grow flex flex-col">
+			<div class="flex flex-grow">
+				<div class="flex-none w-64 bg-gray-800 text-white p-4">
+					<h1 class="text-xl font-bold">Template Selection</h1>
+				</div>
+				<div class="flex-1 bg-white text-gray-800 p-4">
+					<h1 class="text-xl font-bold">Template Editor</h1>
+				</div>
+			</div>
+			<div class="flex flex-grow">
+				<div class="flex-none w-64 bg-gray-700 text-white p-4">
+					<h1 class="text-xl font-bold">Template Parameters</h1>
+				</div>
+				<div class="flex-1 bg-gray-100 text-gray-800 p-4">
+					<h1 class="text-xl font-bold">Template Preview</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
